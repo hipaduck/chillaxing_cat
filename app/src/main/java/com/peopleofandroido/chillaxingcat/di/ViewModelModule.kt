6318e3c7d537230aa46087b0e40782b2d83dbdf0
Gaegul2moja <gaegul2moja@gmail.com>
2022-01-23 17:38:50 +0900

    설정 화면에 디자인 베이스 추가 및 색상 추가/정리

--- app/src/main/java/com/peopleofandroido/chillaxingcat/di/ViewModelModule.kt
+++ app/src/main/java/com/peopleofandroido/chillaxingcat/di/ViewModelModule.kt
@@ -1,10 +1,12 @@
 package com.peopleofandroido.chillaxingcat.di
 
 import com.peopleofandroido.chillaxingcat.presentation.viewmodel.*
+import org.koin.android.ext.koin.androidApplication
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appViewModelModule = module {
     viewModel { MainMenuViewModel(get(), get()) }
+    viewModel { SettingViewModel(androidApplication(), get(), get()) }
     viewModel { SplashViewModel(get()) }
 }
