4ea103cdc80b5e3218643177f4c13beafdc50778
Gaegul2moja <gaegul2moja@gmail.com>
2022-02-07 00:35:30 +0900

    #8 출/퇴근 시간 설정 기능 preference 추가

--- app/src/main/java/com/peopleofandroido/chillaxingcat/ChillaxingCatApplication.kt
+++ app/src/main/java/com/peopleofandroido/chillaxingcat/ChillaxingCatApplication.kt
@@ -1,6 +1,10 @@
 package com.peopleofandroido.chillaxingcat
 
 import android.app.Application
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.peopleofandroido.base.di.componentModule
 import com.peopleofandroido.base.di.networkModule
 import com.peopleofandroido.chillaxingcat.di.appViewModelModule
@@ -9,6 +13,8 @@ import com.peopleofandroido.chillaxingcat.di.useCaseModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+
+internal val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 class ChillaxingCatApplication : Application() {
     override fun onCreate() {
--- app/src/main/java/com/peopleofandroido/chillaxingcat/presentation/viewmodel/SettingViewModel.kt
+++ app/src/main/java/com/peopleofandroido/chillaxingcat/presentation/viewmodel/SettingViewModel.kt
@@ -1,34 +1,123 @@
 package com.peopleofandroido.chillaxingcat.presentation.viewmodel
 
+import android.app.AlarmManager
 import android.app.Application
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.peopleofandroido.base.common.Event
 import com.peopleofandroido.base.common.NavManager
 import com.peopleofandroido.base.util.NotNullMutableLiveData
+import com.peopleofandroido.base.util.logd
+import com.peopleofandroido.chillaxingcat.AlarmReceiver
+import com.peopleofandroido.chillaxingcat.dataStore
 import com.peopleofandroido.chillaxingcat.domain.UseCases
-import com.peopleofandroido.chillaxingcat.domain.model.UserInfoModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.IOException
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class SettingViewModel(
     application: Application,
     private val navManager : NavManager,
     private val useCases: UseCases
 ) : AndroidViewModel(application) {
+    private val keyWorkStartTime = stringPreferencesKey("work_start_time")
+    private val _workStartTime: NotNullMutableLiveData<String> = NotNullMutableLiveData("9:00")
+    val workStartTime: NotNullMutableLiveData<String>
+        get() = _workStartTime
+    private val keyWorkFinishTime = stringPreferencesKey("work_finish_time")
+    private val _workFinishTime: NotNullMutableLiveData<String> = NotNullMutableLiveData("18:00")
+    val workFinishTime: NotNullMutableLiveData<String>
+        get() = _workFinishTime
+
     private val _actionEvent: NotNullMutableLiveData<Event<Action>> = NotNullMutableLiveData(Event(Action()))
     val actionEvent: NotNullMutableLiveData<Event<Action>>
         get() = _actionEvent
+    private var alarmManager: AlarmManager? = null
+    private var mCalender: GregorianCalendar? = null
+
+    init {
+        mCalender = GregorianCalendar()
+
+        loadWorkStartTime()
+        loadWorkFinishTime()
+    }
 
     fun showTimeSettingDialog() {
         _actionEvent.value = Event(Action.DialogAction("time_setting_dialog"))
     }
 
-//    fun getUserInfo() : UserInfoModel {
-//        viewModelScope.launch {
-//            useCases.getUserInfo()
-//        } //???????? 이건 Main에서도 쓸텐데 아예 시간 관리하는 애를 하나 따로 뺄까 (db 구성부터도 고민이 필요할 것 같음)
-//    }
+    private fun loadWorkStartTime() {
+        viewModelScope.launch {
+            val data = getApplication<Application>().dataStore.data
+                .catch { exception ->
+                    if (exception is IOException) {
+                        emit(emptyPreferences())
+                    } else {
+                        throw exception
+                    }
+                }.map { preferences ->
+                    preferences[keyWorkStartTime] ?: "9:00"
+                }
+            workStartTime.value = data.first()
+            logd("Load workStartTime: ${workStartTime.value}")
+        }
+    }
 
+    private fun loadWorkFinishTime() {
+        viewModelScope.launch {
+            val data = getApplication<Application>().dataStore.data
+                .catch { exception ->
+                    if (exception is IOException) {
+                        emit(emptyPreferences())
+                    } else {
+                        throw exception
+                    }
+                }.map { preferences ->
+                    preferences[keyWorkFinishTime] ?: "18:00"
+                }
+            workFinishTime.value = data.first()
+            logd("Load workFinishTime: ${workFinishTime.value}")
+        }
+    }
+
+    fun storeWorkStartTime(startTime : String) {
+        viewModelScope.launch {
+            getApplication<Application>().dataStore.edit { preferences ->
+                preferences[keyWorkStartTime] = startTime
+                withContext(Dispatchers.Main) {
+                    workStartTime.value = startTime
+                    logd("Stored workStartTime: " + workStartTime.value)
+                }
+            }
+        }
+    }
+
+    fun storeWorkFinishTime(finishTime : String) {
+        viewModelScope.launch {
+            getApplication<Application>().dataStore.edit { preferences ->
+                preferences[keyWorkFinishTime] = finishTime
+                withContext(Dispatchers.Main) {
+                    workFinishTime.value = finishTime
+                    logd("Stored workFinishTime: " + workFinishTime.value)
+                }
+            }
+        }
+    }
+    
     open class Action {
         class DialogAction(val type: String): Action()
     }
