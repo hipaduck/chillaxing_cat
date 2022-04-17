1f4ea252d00a6cfa48fd94929abe202c98c3a1c8
Soobin Park <binsoopark@gmail.com>
2022-04-17 20:53:54 +0900

    #24 메인화면 공휴일 디데이 표현기능 추가

--- app/src/main/java/com/peopleofandroido/chillaxingcat/presentation/viewmodel/MainMenuViewModel.kt
+++ app/src/main/java/com/peopleofandroido/chillaxingcat/presentation/viewmodel/MainMenuViewModel.kt
@@ -4,17 +4,23 @@ import android.app.Application
 import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import com.kizitonwose.calendarview.utils.yearMonth
 import com.peopleofandroido.base.common.Event
 import com.peopleofandroido.base.common.NavManager
+import com.peopleofandroido.base.domain.Status
 import com.peopleofandroido.base.util.NotNullMutableLiveData
 import com.peopleofandroido.base.util.logd
+import com.peopleofandroido.base.util.loge
 import com.peopleofandroido.chillaxingcat.R
 import com.peopleofandroido.chillaxingcat.domain.UseCases
 import com.peopleofandroido.chillaxingcat.presentation.ui.MainMenuFragmentDirections
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 
 class MainMenuViewModel(
     application: Application,
@@ -38,10 +44,17 @@ class MainMenuViewModel(
     val isStopButtonVisible: NotNullMutableLiveData<Boolean>
         get() = _isStopButtonVisible
 
+    private val _holidayDDayFromToday: NotNullMutableLiveData<String> = NotNullMutableLiveData("")
+    val holidayDDayFromToday: NotNullMutableLiveData<String>
+        get() = _holidayDDayFromToday
+
+    private var _criteriaDDayDate: LocalDate = LocalDate.now()
+
 
     init {
         checkFirstTime()
         checkTodayState()
+        updateDecimalDayFromClosestHoliday()
     }
 
     fun moveToSetting() {
@@ -242,6 +255,63 @@ class MainMenuViewModel(
                 } else {
                     Toast.makeText(getApplication(), getApplication<Application>().getText(R.string.main_resting_finished_toast_message), Toast.LENGTH_SHORT).show()
                 }
+            }
+        }
+    }
+
+    private fun updateDecimalDayFromClosestHoliday() {
+        // 0. 갱신해야 하는지 여부를 체크(D-Day값이 비어있지 않고, 기준날짜가 오늘과 같을 경우는 갱신 안함)
+        if (_holidayDDayFromToday.value.isNotEmpty() && _criteriaDDayDate == LocalDate.now()) return
+
+        // 1. 오늘 날짜를 처음 기준으로 설정
+        var currentDay = LocalDate.now()
+        val holidayList = mutableListOf<LocalDate>()
+        viewModelScope.launch(Dispatchers.IO) {
+            // 2. 공휴일 List 안에 1개 미만인 경우, 계속 얻어오기 시도
+            while (holidayList.size < 1) {
+                // 3. 해당월의 공휴일 얻어오기
+                val yyyyMM = currentDay.format(DateTimeFormatter.ofPattern("yyyyMM"))
+                val result = useCases.getHolidayWithPeriod("${yyyyMM.substring(0,4)}${yyyyMM.substring(4,6)}",
+                    "${yyyyMM.substring(0,4)}${yyyyMM.substring(4,6)}")
+
+                when (result.status) {
+                    Status.SUCCESS -> {
+                        logd("list: ${result.data?.size}")
+                        result.data?.let {
+                            for (dateModel in it) {
+                                if (dateModel.id.toString().length >= 8) {
+                                    val localDate = LocalDate.parse(dateModel.id.toString(), DateTimeFormatter.ofPattern("yyyyMMdd"))
+                                    logd("localDate: $localDate")
+                                    holidayList.add(localDate)
+                                }
+                            }
+                        }
+                    }
+                    Status.ERROR -> {
+                        loge("error: ${result.message}")
+                    }
+                }
+
+                // 4. 만약 currentMonth가 현재 달과 같다면, 오늘날짜를 포함하여 이후날짜만 포함시킴
+                if (currentDay.yearMonth == LocalDate.now().yearMonth) {
+                    holidayList.filter { it >= LocalDate.now() }
+                }
+
+                // 5. 공휴일이 1개 이상일 경우 오른차순 정렬, 아니라면 currentDay를 한 달 증가시킴
+                if (holidayList.size > 1) {
+                    holidayList.sort()
+                } else {
+                    currentDay = currentDay.plusMonths(1)
+                }
+
+                // 6. 1년 이상의 탐색은 의미가 없으므로, 1년이 넘도록 값을 조회하지 못한다면, 중단함
+                if (ChronoUnit.MONTHS.between(LocalDate.now(), currentDay) > 12) break
+            }
+
+            // 7. 가장 가까운 공휴일과 오늘의 차이를 구해서 D-Day에 반영 및 기준일자도 갱신해주면 마무리
+            withContext(Dispatchers.Main) {
+                _holidayDDayFromToday.value = ChronoUnit.DAYS.between(LocalDate.now(), holidayList[0]).toString()
+                _criteriaDDayDate = LocalDate.now()
             }
         }
     }
